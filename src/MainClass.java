2229e60779abb835461888785696955d76e80be7
Roman Kuznetsov <57463913+romankuzn@users.noreply.github.com>
2020-09-07 09:16:49 -0400

    Homework. Unit: 0 Test: 3
    Homework
    Unit: 0
    Test: 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,11 @@
 public class MainClass {
 
     private int class_number = 46;
+    private String class_string ="Mello, world";
+
+    public String getClass_string() {
+        return class_string;
+    }
 
     public int getClass_number() {
         return class_number;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 public class MainClassTest {
 
     @Test
+    public void testGetClassString() {
+        MainClass main = new MainClass();
+        String returnedString = main.getClass_string();
+        boolean isFound = returnedString.contains("Hello") || returnedString.contains("hello");
+        Assert.assertTrue("Hello or hello is NOT returned by getClassString", isFound);
+    }
+
+    @Test
     public void testGetClassNumber() {
         MainClass main = new MainClass();
         Assert.assertTrue("getClassNumber returns value <= 45",main.getClass_number() > 45);
