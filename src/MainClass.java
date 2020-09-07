8216b698d277a49e2862d23fc5b7ddebd3e2e5ee
Roman Kuznetsov <57463913+romankuzn@users.noreply.github.com>
2020-09-07 08:48:48 -0400

    Homework. Unit: 0 Test: 2
    Homework
    Unit: 0
    Test: 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,11 @@
 public class MainClass {
+
+    private int class_number = 46;
+
+    public int getClass_number() {
+        return class_number;
+    }
+
     public int getLocalNumber() {
         return 13;
     }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 public class MainClassTest {
 
     @Test
+    public void testGetClassNumber() {
+        MainClass main = new MainClass();
+        Assert.assertTrue("getClassNumber returns value <= 45",main.getClass_number() > 45);
+    }
+
+    @Test
     public void testGetLocalNumber() {
         MainClass main = new MainClass();
         Assert.assertTrue("getLocalNumber does NOT return 14", main.getLocalNumber() == 14);
